f2362e04668fb8a7c6efc74438a076a27364ecab
rodgerswisdom <trulyhawona@gmail.com>
2024-07-17 01:28:10 +0300

    feat: route launchscreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'src/ui/splash/launchscreen.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,15 +9,52 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('My First Flutter App'),
-        ),
-        body: Center(
-          child: Text(
-            'Hello, Flutter!',
-            style: TextStyle(fontSize: 24),
-          ),
+      title: 'My First Flutter App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: SplashScreenWrapper(),
+    );
+  }
+}
+
+class SplashScreenWrapper extends StatefulWidget {
+  @override
+  _SplashScreenWrapperState createState() => _SplashScreenWrapperState();
+}
+
+class _SplashScreenWrapperState extends State<SplashScreenWrapper> {
+  @override
+  void initState() {
+    super.initState();
+    _navigateToHome();
+  }
+
+  _navigateToHome() async {
+    await Future.delayed(Duration(seconds: 3), () {});
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => HomeScreen()),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SplashScreen();
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My First Flutter App'),
+      ),
+      body: Center(
+        child: Text(
+          'Hello, Flutter!',
+          style: TextStyle(fontSize: 24),
         ),
       ),
     );
