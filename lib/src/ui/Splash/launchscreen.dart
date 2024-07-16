1a0864aff6fbfe3e0119fc3aa6f154e97e47cc2a
rodgerswisdom <trulyhawona@gmail.com>
2024-07-17 00:46:54 +0300

    feat: add launchscreen

--- lib/src/ui/Splash/launchscreen.dart
+++ lib/src/ui/Splash/launchscreen.dart
@@ -1,3 +1,23 @@
 /**
  * launchscreen
  */
+
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FlutterLogo(size: 100), // Replace with your logo
+            SizedBox(height: 20),
+            CircularProgressIndicator(),
+          ],
+        ),
+      ),
+    );
+  }
+}
