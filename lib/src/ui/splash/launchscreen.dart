032c01b4ecc04f9c5ee0a2d357361a13a2648ca4
rodgerswisdom <trulyhawona@gmail.com>
2024-07-20 00:03:48 +0300

    conflict

---
+++ lib/src/ui/splash/launchscreen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.blue,
+      body: Center(
+        child: Text(
+          'BoB',
+          style: TextStyle(
+            fontSize: 40,
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
