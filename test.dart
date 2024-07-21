68a57c42094a95bd0c6565a9882c1ac43d6438bc
rodgerswisdom <trulyhawona@gmail.com>
2024-07-21 22:08:52 +0300

    refactor all

--- test.dart
+++ test.dart
@@ -1,38 +1,38 @@
-import 'package:bob/src/ui/assessment/assessment.dart';
-import 'package:flutter/material.dart';
+// import 'package:bob/src/ui/assessment/assessment.dart';
+// import 'package:flutter/material.dart';
 
-class SplashScreen extends StatefulWidget {
-  const SplashScreen({super.key});
+// class SplashScreen extends StatefulWidget {
+//   const SplashScreen({super.key});
 
-  @override
-  State<SplashScreen> createState() => _SplashScreenState();
-}
+//   @override
+//   State<SplashScreen> createState() => _SplashScreenState();
+// }
 
-class _SplashScreenState extends State<SplashScreen> {
-  @override
-  void initState() {
-    super.initState();
-    Future.delayed(const Duration(seconds: 3), () {
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => const AssessmentScreen()),
-      );
-    });
-  }
+// class _SplashScreenState extends State<SplashScreen> {
+//   @override
+//   void initState() {
+//     super.initState();
+//     Future.delayed(const Duration(seconds: 3), () {
+//       Navigator.pushReplacement(
+//         context,
+//         MaterialPageRoute(builder: (context) => const AssessmentScreen()),
+//       );
+//     });
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            // Add your splash screen content (logo, text, etc.)
-            Text('BoB'),
-            CircularProgressIndicator(),
-          ],
-        ),
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Scaffold(
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             // Add your splash screen content (logo, text, etc.)
+//             Text('BoB'),
+//             CircularProgressIndicator(),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
