d45af6a99f98ddfe6909a44071dbc5a4b35997d6
rodgerswisdom <trulyhawona@gmail.com>
2024-07-23 22:24:50 +0300

    feat: Implement questions page

---
+++ lib/screens/question_page.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import '../widgets/progress_bar.dart';
+import '../widgets/question_widget.dart';
+
+class QuestionPage extends StatefulWidget {
+  final Map<String, dynamic> questions;
+  final List<String> questionIds;
+  final Function(String, String) onAnswerSelected;
+  final Function onCompleted;
+
+  QuestionPage({
+    required this.questions,
+    required this.questionIds,
+    required this.onAnswerSelected,
+    required this.onCompleted,
+  });
+
+  @override
+  _QuestionPageState createState() => _QuestionPageState();
+}
+
+class _QuestionPageState extends State<QuestionPage> {
+  int _currentQuestionIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    final questionId = widget.questionIds[_currentQuestionIndex];
+    final question = widget.questions[questionId];
+
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Text(
+            'Category: ${question['category']}',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+          ProgressBar(
+            progress: (_currentQuestionIndex + 1) / widget.questionIds.length,
+          ),
+          SizedBox(height: 16.0),
+          QuestionWidget(
+            question: question,
+            onSelected: (choice) {
+              widget.onAnswerSelected(question['id'], choice);
+              if (_currentQuestionIndex < widget.questionIds.length - 1) {
+                setState(() {
+                  _currentQuestionIndex++;
+                });
+              } else {
+                widget.onCompleted();
+              }
+            },
+          ),
+          SizedBox(height: 20.0),
+         
+        ],
+      ),
+    );
+  }
+}
