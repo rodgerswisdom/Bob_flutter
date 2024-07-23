00498833c4f0218417e0e7b1190ebc0151d3f27b
rodgerswisdom <trulyhawona@gmail.com>
2024-07-24 00:38:31 +0300

    delete: user_service for assessments

--- lib/services/user_service2.dart
+++
@@ -1,8 +0,0 @@
-import 'package:shared_preferences/shared_preferences.dart';
-
-class UserService {
-  static Future<String?> getUserId() async {
-    final prefs = await SharedPreferences.getInstance();
-    return prefs.getString('userId');
-  }
-}
