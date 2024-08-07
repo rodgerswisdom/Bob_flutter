e89841f4f6408d123fc1644e58daa96543f46771
Ridan Hawy <113379842+Hawy08@users.noreply.github.com>
2024-08-07 13:45:20 +0300

    Assesment styling

--- lib/widgets/progress_bar.dart
+++ lib/widgets/progress_bar.dart
@@ -2,11 +2,24 @@ import 'package:flutter/material.dart';
 
 class ProgressBar extends StatelessWidget {
   final double progress;
+  final Color color;
 
-  const ProgressBar({super.key, required this.progress});
+  const ProgressBar({
+    required this.progress,
+    this.color = const Color(0xFF2259AB), // Default color
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return LinearProgressIndicator(value: progress);
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10.0),
+      child: LinearProgressIndicator(
+        value: progress,
+        backgroundColor: Colors.grey[300],
+        valueColor: AlwaysStoppedAnimation<Color>(color),
+        minHeight: 10.0,
+      ),
+    );
   }
 }
