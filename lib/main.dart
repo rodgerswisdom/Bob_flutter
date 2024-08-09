f20d9755135b7e269c018bb67e7183e248e1be52
rodgerswisdom <trulyhawona@gmail.com>
2024-08-10 01:18:22 +0300

    refactor: Remove Debug mode banner

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'BoB Financial Literacy App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
