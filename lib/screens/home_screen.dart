c698bb21b67d9fd11182b81447ea57fa79d661b9
Ridan Hawy <113379842+Hawy08@users.noreply.github.com>
2024-08-12 16:05:24 +0300

    Scrollable homepage

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -50,84 +50,94 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        toolbarHeight: 240,
-        shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.vertical(
-            bottom: Radius.circular(20),
-          ),
-        ),
-        backgroundColor: const Color(0xFF2259AB), // Original AppBar color
-        centerTitle: true, // Center the title content
-        title: Container(
-          padding: const EdgeInsets.all(16.0),
-          decoration: BoxDecoration(
-            color: Color.fromARGB(
-                255, 71, 124, 203), // User info box color with 50% opacity
-            borderRadius: BorderRadius.circular(10.0),
-          ),
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              CircleAvatar(
-                backgroundColor: Colors.grey[300], // Placeholder avatar color
-                child: const Icon(Icons.person, color: Colors.white),
-                radius: 30, // Avatar size
-              ),
-              const SizedBox(width: 20),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    _getGreeting(),
-                    style: const TextStyle(fontSize: 20, color: Colors.white),
-                  ),
-                  Text(
-                    _displayName,
-                    style: const TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ),
-      ),
       body: _isLoading
           ? const Center(child: CircularProgressIndicator())
-          : const SingleChildScrollView(
-              child: Padding(
-                padding: EdgeInsets.all(16.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    SizedBox(height: 20),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          : CustomScrollView(
+              slivers: [
+                SliverAppBar(
+                  expandedHeight: 240.0,
+                  floating: true,
+                  pinned: true,
+                  flexibleSpace: FlexibleSpaceBar(
+                    background: Container(
+                      decoration: const BoxDecoration(
+                        color: Color(
+                            0xFF2259AB), // User info box color with 50% opacity
+                        borderRadius: BorderRadius.vertical(
+                          bottom: Radius.circular(20),
+                        ),
+                      ),
+                      child: Center(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            CircleAvatar(
+                              backgroundColor:
+                                  Colors.grey[300], // Placeholder avatar color
+                              child:
+                                  const Icon(Icons.person, color: Colors.white),
+                              radius: 30, // Avatar size
+                            ),
+                            const SizedBox(width: 20),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Text(
+                                  _getGreeting(),
+                                  style: const TextStyle(
+                                      fontSize: 20, color: Colors.white),
+                                ),
+                                Text(
+                                  _displayName,
+                                  style: const TextStyle(
+                                      fontSize: 24,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  backgroundColor:
+                      const Color(0xFF2259AB), // Original AppBar color
+                ),
+                const SliverToBoxAdapter(
+                  child: Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Expanded(
-                          child: SizedBox(
-                            height: 255,
-                            child:
-                                GoalsCard(), // Ensure GoalsCard is properly implemented
-                          ),
+                        SizedBox(height: 20),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Expanded(
+                              child: SizedBox(
+                                height: 255,
+                                child:
+                                    GoalsCard(), // Ensure GoalsCard is properly implemented
+                              ),
+                            ),
+                            SizedBox(width: 16.0),
+                            Expanded(
+                              child: SizedBox(
+                                height: 255,
+                                child: SavingsWidget(),
+                              ),
+                            ),
+                          ],
                         ),
-                        SizedBox(width: 16.0),
-                        Expanded(
-                          child: SizedBox(
-                            height: 255,
-                            child: SavingsWidget(),
-                          ),
-                        ),
+                        SizedBox(height: 20),
+                        ModuleWidget(),
                       ],
                     ),
-                    SizedBox(height: 20),
-                    ModuleWidget(),
-                  ],
+                  ),
                 ),
-              ),
+              ],
             ),
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
